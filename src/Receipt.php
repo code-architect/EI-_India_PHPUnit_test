7312e175f565f0952798cc9ead093d7b474c0d31
Indranil Samanta <indranileiindia2019@gmail.com>
2019-04-10 12:16:31 +0530

    added the tax method in Receipt class

--- src/Receipt.php
+++ src/Receipt.php
@@ -4,12 +4,23 @@ namespace TDD;
 class Receipt
 {
     /**
-     * Sum up the values and return 
+     * Sum up the values and return
      * @param array $items
      * @return float|int
      */
     public function total(array $items = [])
     {
         return array_sum($items);
+    }
+
+    /**
+     * Calculate the tax amount
+     * @param $amount
+     * @param $taxPercentage
+     * @return mixed
+     */
+    public function tax($amount, $taxPercentage)
+    {
+        return ($amount * $taxPercentage);
     }
 }
